5d977b2ca5135109efa4a6e9eba96984a8ac5d5d
Samsar4 <samsar4sec@gmail.com>
2021-03-29 11:43:29 +0100

    9/100

--- src/goroutines.go
+++ src/goroutines.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"time"
 )
 
 // Goroutines are functions or methods that run concurrently with other functions or methods.
 // Goroutines can be thought of as light weight threads.
 // The cost of creating a Goroutine is tiny when compared to a thread. Hence it's common for Go applications to have thousands of Goroutines running concurrently.
 
-func main() {
+func message(str string) {
+	for i := 0; i <= 3; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(str, i)
+	}
+}
 
+func main() {
+	go message("Yo!")
+	message("Yo2")
 }
